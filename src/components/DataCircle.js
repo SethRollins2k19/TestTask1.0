2a27471511ab91f710aecd986399815b0d3beb09
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2020-04-08 15:52:48 +0300

    last update

--- src/components/DataCircle.js
+++ src/components/DataCircle.js
@@ -6,8 +6,54 @@ export default class DataCircleComponent extends React.Component {
     state = {
         count: 1,
         countOfTitled: 1,
-
+        objectHovered: false,
     }
+    // initTimer(){
+    //     let timer;
+    //     function handleTimer(handleFunc,delay){
+    //         clearInterval(timer);
+    //         timer = setInterval(handleFunc,delay);
+    //     }
+    //     function clearTimer(){
+    //         clearInterval(timer)
+    //     }
+    //     return{
+    //         handleTimer,
+    //         clearTimer
+    //     }
+    // }
+    // handleRotate(item){
+    //
+    // }
+    componentDidMount() {
+        this.item = document.querySelector(`.${style.Circle}`);
+        let item = this.item;
+        this.radian = 0;
+        this.timer = setInterval(()=>{
+            item.style.transform = `rotateZ(${this.radian}deg)`;
+            this.radian +=1;
+            if(this.radian === 360){
+                this.radian = 0;
+            }
+        },50)
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        let item = this.item;
+        clearInterval(this.timer);
+        if(prevState.count !== this.state.count || prevState.countOfTitled !== this.state.countOfTitled){
+           this.radian = 0;
+        }
+        if(!this.state.objectHovered){
+            this.timer = setInterval(()=>{
+                item.style.transform = `rotateZ(${this.radian}deg)`;
+                this.radian +=1;
+                if(this.radian === 360){
+                    this.radian = 0;
+                }
+            },50)
+        }
+    }
+
     render() {
 
         const items = this.props.items;
@@ -22,7 +68,15 @@ export default class DataCircleComponent extends React.Component {
                     {items.map((item, index) => {
                         if(index<count) {
                             return (
-                                <DataCircleItemComponent key={index} item={item} index={index} titled={countOfTitled>index}/>
+                                <DataCircleItemComponent key={index}
+                                                         item={item}
+                                                         index={index}
+                                                         titled={countOfTitled>index}
+                                                         radian={this.radian}
+                                                         timer={this.timer}
+                                                         objectHovered={this.state.objectHovered}
+                                                         changeHovered={()=>this.setState(prevState=>({...prevState,objectHovered: !prevState.objectHovered}))}
+                                />
                             )
                         } else {
                             return ""
@@ -57,4 +111,27 @@ export default class DataCircleComponent extends React.Component {
         )
     }
 }
+
+// class Rotate {
+//     constructor(props){
+//         this.raidan = props.radian;
+//         this.delay = props.delay;
+//         this.item = props.item;
+//     }
+//     handleRotation(){
+//         let delay = this.delay;
+//         let item = this.item;
+//         this.timer = setInterval(()=>{
+//
+//             item.style.transform = `rotateZ(${this.raidan}deg)`
+//         },delay)
+//     }
+// }
+
+
+
+
+
+
+
 
--- src/components/DataCircleItem.js
+++ src/components/DataCircleItem.js
@@ -6,25 +6,59 @@ export default class DataCircleItemComponent extends React.Component {
         let item = document.querySelector(`.circleItem-${this.props.index}`);
         let {height, width } = item.parentNode.getBoundingClientRect();
         let x = rand(0,width);
-        x = x > width/2 ? x - item.offsetWidth/2: x + item.offsetWidth;
+        x = x > width/2 ? x - item.offsetWidth: x + item.offsetWidth/2;
         let y = rand(0,height);
-        y = y > height/2 ? y - item.offsetHeight: y + item.offsetHeight/2;
+        y = y > height/2 ? y - item.offsetHeight: y + item.offsetHeight;
 
         while(!isInCircle(width/2,height/2,x,y)){
             x = rand(0,width);
-            x = x > width/2 ? x - item.offsetWidth/2: x + item.offsetWidth/2;
+            x = x > width/2 ? x - item.offsetWidth: x + item.offsetWidth/2;
             y = rand(0,height);
-            y = y > height/2 ? y - item.offsetHeight/2: y + item.offsetHeight/2;
+            y = y > height/2 ? y - item.offsetHeight: y + item.offsetHeight;
 
         }
         item.style.left = x + "px";
         item.style.top = y + 'px';
-
+        this.radian = -this.props.radian;
+        if(!this.props.objectHovered){
+            this.timer = setInterval(()=>{
+                item.style.transform = `rotateZ(${this.radian}deg)`
+                this.radian-=1;
+                if(this.radian === -360){
+                    this.radian = 0;
+                }
+            },50)
+        }
+        item.addEventListener('mouseover',(e)=>{
+            this.props.changeHovered();
+        })
+        item.addEventListener('mouseout',(e)=>{
+            this.props.changeHovered();
+        })
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        let item = document.querySelector(`.circleItem-${this.props.index}`);
+        clearInterval(this.timer)
+        if(!this.props.objectHovered){
+            this.radian = -this.props.radian;
+            this.timer = setInterval(()=>{
+                item.style.transform = `rotateZ(${this.radian}deg)`
+                this.radian-=1;
+                if(this.radian === -360){
+                    this.radian = 0;
+                }
+            },50)
+        }
     }
 
     render() {
         const{title = "", link = "#", filled = false} = this.props.item;
         const titled = this.props.titled;
+
+
+
+
+
         return (
             <a href={link} className={`${style.CircleItem} circleItem-${this.props.index}`} >
                 <div className={`${style.CircleShape}  ${filled ? style.CircleItemBlack : ""}`}>
